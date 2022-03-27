4d66af6a1f7b9f5071068247d5982081a29586c8
Josh Buegner <josh.buegner@gmail.com>
2022-03-26 21:39:02 -0400

    Mission Accomplished

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let multiply = 62
+
+multiply = num1 * num2 
+
+
+console.log(random)
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 16
+let num4 = 4
+let mod = 4
+
+mod = num3 / num4
+
+let max = Math.max(20) 
